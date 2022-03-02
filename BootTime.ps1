6e40281530aaccf68e0ae518f99694919ad02cd2
Jon Boyette <98356454+Dunforever@users.noreply.github.com>
2022-03-02 14:22:37 -0600

    Add files via upload

---
+++ BootTime.ps1
@@ -0,0 +1,15 @@
+﻿# Powershell script to check Last Reboot Time on a list of machines included in a text file and export the report as a CSV file 
+# Author - Vikram Bedi  
+# vikram.bedi.it@gmail.com  
+ 
+ 
+$machines = Get-Content C:\Temp\Boot_List.txt 
+$report = @() 
+$object = @() 
+foreach($machine in $machines) 
+{ 
+$machine 
+$object = gwmi win32_operatingsystem -ComputerName $machine | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} 
+$report += $object 
+} 
+$report | Export-csv C:\Temp\Reboot.csv
