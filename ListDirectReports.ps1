5aa5444c00a6903e149a3dba4e6bb645a177cc32
Jon Boyette <98356454+Dunforever@users.noreply.github.com>
2022-03-02 10:28:49 -0600

    Add files via upload

---
+++ ListDirectReports.ps1
@@ -0,0 +1,9 @@
+﻿Clear-host
+Import-Module ActiveDirectory
+Write-Host . LIST DIRECT REPORTS . -ForegroundColor white -BackgroundColor red
+Write-Host
+$Manager = Read-Host [ Enter Manager User Name ]
+Write-Host
+Write-Host ... These are Direct Reports to $Manager -ForegroundColor yellow
+Write-Host
+Get-ADUser -Identity $Manager -Properties directreports | select-object -ExpandProperty DirectReports
