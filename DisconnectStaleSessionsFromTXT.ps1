f890ca13c3051b43ac247e24b738fcc754ae1910
Jon Boyette <98356454+Dunforever@users.noreply.github.com>
2022-03-02 11:01:32 -0600

    Add files via upload

---
+++ DisconnectStaleSessionsFromTXT.ps1
@@ -0,0 +1,11 @@
+﻿$ServerList = Get-Content C:\temp\Servers.txt
+foreach ($server in $ServerList)
+{
+quser /server:$server | ? { $_ -match “Disc” }|foreach {
+$Session = ($_ -split ‘ +’)[2]
+$user = ($_ -split ‘ +’)[1]
+$idletime= ($_ -split ‘ +’)[4]
+Write-host “You are about to log off user $user with session id $Session who is idle for $idletime ”
+logoff $Session /server:$server
+}
+}
