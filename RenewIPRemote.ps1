efe568f84dd491fa5a4120ddc2a89ef8c0f23c49
Jon Boyette <98356454+Dunforever@users.noreply.github.com>
2022-03-02 11:00:13 -0600

    Add files via upload

---
+++ RenewIPRemote.ps1
@@ -0,0 +1,6 @@
+﻿write-host “—————————–”
+write-host “DHCP lease renewal + flushDNS”
+write-host “—————————–”
+$machine = read-host “IP/Name of machine needing lease renewal ? ”
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $machine | Where { $_.IpEnabled -eq $true -and $_.DhcpEnabled -eq $true} | ForEach-Object -Process {$_.RenewDHCPLease()}
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $machine | Where { $_.IpEnabled -eq $true -and $_.DhcpEnabled -eq $true} | ForEach-Object -Process {ipconfig /flushdns}
