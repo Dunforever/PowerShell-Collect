2ef4395815f0e9d821eb414b68001fd43fb9448a
Jon Boyette <98356454+Dunforever@users.noreply.github.com>
2022-03-02 15:13:32 -0600

    Add files via upload

---
+++ GetLocalMetrics.ps1
@@ -0,0 +1,18 @@
+﻿  [CmdletBinding()]
+  param (
+    [ValidateSet ("IPv4","IPv6")]
+    [String]$AddressFamily
+  )
+  
+  Get-NetIPInterface @PSBoundParameters | Sort-Object InterfaceMetric | ForEach-Object {
+    if($_.InterfaceAlias -eq 1) { return; }
+    $adapter= Get-NetAdapter
+    $address=$_ | Get-NetIPAddress
+       [PSCustomObject]@{
+      InterfaceAlias=$_.InterfaceAlias;
+      InterfaceIndex=$_.InterfaceIndex;
+      Address="$($address.IPAddress)/$($address.PrefixLength)"
+      Metric=$_.InterfaceMetric;
+      Speed=$adapter.LinkSpeed
+         }
+  } | ft
